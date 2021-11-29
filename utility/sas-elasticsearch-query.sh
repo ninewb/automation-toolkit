72786c12b85aa70e1cf54a4fafb5328f84d9ca16
Nick Newbill <nick.newbill@sas.com>
2021-11-29 18:32:10 +0000

    elastic query changes

--- utility/sas-elasticsearch-query.sh
+++ utility/sas-elasticsearch-query.sh
@@ -1,8 +1,21 @@
 
-export host=psfc.unx.sas.com
-export cacert=/opt/sas/viya/config/etc/SASSecurityCertificateFramework/cacerts/trustedcerts.pem
-export key=/opt/sas/viya/config/etc/elasticsearch/default/keys/searchguard/sgadminkey.pem
-export cert=/opt/sas/viya/config/etc/elasticsearch/default/certs/searchguard/sgadmincert.pem
+export host=$(hostname -f)
+export vbase=/opt/sas/viya/config
+export cacert=${vbase}/etc/SASSecurityCertificateFramework/cacerts/trustedcerts.pem
+
+# Lower than VI 10.8
+
+if [ -d /etc/elasticsearch/default/keys/searchguard ]; then
+  export key=${vbase}/etc/elasticsearch/default/keys/searchguard/sgadminkey.pem
+  export cert=${vbase}/etc/elasticsearch/default/certs/searchguard/sgadmincert.pem
+fi
+
+# VI 10.8+
+
+if [ -d ${vbase}/etc/elasticsearch/default/certs/opendistro ]; then
+  export cert=${vbase}/etc/elasticsearch/default/certs/opendistro/opendistrohealthcheck-cert.pem
+  export key=${vbase}/etc/elasticsearch/default/keys/opendistro/opendistrohealthcheck-key.pem
+fi
 
 sudo curl \
   --cacert ${cacert} \
