f2026a667bd4487852b153f338b60099aa0b557b
Nick Newbill <nick.newbill@sas.com>
2022-08-09 10:16:22 -0400

    Update base-resize-lvm.sh

--- utility/base-resize-lvm.sh
+++ utility/base-resize-lvm.sh
@@ -1,1 +1,9 @@
-
+sudo vgdisplay rootvg
+sudo yum install cloud-utils-growpart gdisk -y
+sudo pvscan
+lsblk /dev/sda2
+sudo growpart /dev/sda 2
+lsblk /dev/sda2
+sudo pvresize /dev/sda2
+sudo pvscan
+sudo lvresize -r -L +135G /dev/mapper/rootvg-rootlv
